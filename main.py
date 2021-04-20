5f4d20c800913433ee2b649a06e55666fb163637
turkishbanking <82497239+turkishbanking@users.noreply.github.com>
2021-04-20 20:27:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def write_file(arg: str) -> None:
 class Checker:
     HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
     def __init__(self, usernames: List[str]):
-        self.to_check = usernames
+        self.to_check = hsyinaltindag
         #self.count = 0
 
     async def _check(self, session: aiohttp.ClientSession, username: str) -> None:     
